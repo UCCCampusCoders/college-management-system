e07243f768e582e5d2e11f2fc89060e2f5125986
dhananjay2107 <dhananjayjshenoy21@gmail.com>
2025-05-22 11:09:01 +0530

    included routers in main python file

--- college-core/college/main.py
+++ college-core/college/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, Response
 from college.db.database import get_db
+from college.routes import auth, batch, course, faculty, program, student, user
 from college.utils.request_logging_middleware import RequestLoggingMiddleware
 from college.core.logging_config import app_logger
 
@@ -33,3 +34,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 app.add_middleware(RequestLoggingMiddleware)
+
+app.include_router(program.router, prefix="/program", tags=["Program"])
+app.include_router(course.router, prefix="/course", tags=["Course"])
+app.include_router(student.router, prefix="/student", tags=["Student"])
+app.include_router(faculty.router, prefix="/faculty", tags=["Faculty"])
+app.include_router(batch.router, prefix="/batch", tags=["Batch"])
+app.include_router(user.router, prefix="/user", tags=["User"])
+app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the College Management Backend"}
